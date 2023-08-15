d7c2cdb2b1b9cf536b6143bf7a1103cc770d84f7
huseyin corakli <huseyincorakli@icloud.com>
2023-08-16 02:13:26 +0300

    .

---
+++ Problem_3/Program.cs
@@ -0,0 +1,4 @@
+﻿// Verilen bir dizide, dizinin içindeki bir sayıyı hedef sayıya ulaşmak için toplama ve çıkarma işlemleri yaparak kullanmanız isteniyor. Verilen dizideki sayıları istediğiniz gibi işlem yaparak kullanarak hedef sayıya ulaşmanın mümkün olup olmadığını dönderiniz
+
+
+   //çözemedim
